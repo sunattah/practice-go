e7eaa39ac40675896b03cd4d8127f20b66d3e0aa
sunattah <sundayattah319@gmail.com>
2026-07-28 15:10:40 +0100

    my python tuple

--- practicefolder/tuple_file.py
+++ practicefolder/tuple_file.py
@@ -1,6 +1,16 @@
 # fruits = ("apple", "banana", "cherry", "dobinu")
 # x,y,z,w = fruits
-# print(y)
-fruits = ("apple", "banana", "cherry", "dobinu")
-x,y,z,*w = fruits
-print(w)
+# # print(y)
+# fruits = ("apple", "banana", "cherry", "dobinu")
+# # x,*w = fruits
+# # print(w)
+
+# for x  in fruits:
+#    print(x)
+
+
+mytuple = ('apple', 'banana', 'cherry')
+i = 0
+while i < len((mytuple)):
+  print(mytuple[i])
+  i = i + 1
