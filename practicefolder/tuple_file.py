5f9c3a36f9991e1e2f75a4d7029596dc13a75837
sunattah <sundayattah319@gmail.com>
2026-07-28 14:44:47 +0100

    my python tuple

--- practicefolder/tuple_file.py
+++ practicefolder/tuple_file.py
@@ -1,3 +1,6 @@
+# fruits = ("apple", "banana", "cherry", "dobinu")
+# x,y,z,w = fruits
+# print(y)
 fruits = ("apple", "banana", "cherry", "dobinu")
-x,y,z,w = fruits
-print(y)
+x,y,z,*w = fruits
+print(w)
